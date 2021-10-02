4fdafcce0f16849401b41ea22c24a28ad63e83de
lopeselio <lopeselio@gmail.com>
2021-10-02 11:56:09 +0530

    added tailwind to the project

---
+++ craco.config.js
@@ -0,0 +1,21 @@
+// craco.config.js
+module.exports = {
+    style: {
+      postcss: {
+        plugins: [
+          require('tailwindcss'),
+          require('autoprefixer'),
+        ],
+      },
+    },
+  }// craco.config.js
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+}
--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" class="p-6 items-center justify-center">
+        <h1 class="text-blue-400 font-extrabold">Hello World!</h1>
+        <p class="tracking-widest">This is my first React App.</p>
     </div>
   );
 }
