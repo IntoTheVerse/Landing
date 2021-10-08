dd7edcdc384591a78975aa459d987c031806d029
madrigal1 <kurupgokul11@gmail.com>
2021-10-08 21:36:24 +0530

    feat: loading page

--- src/pages/LoadingScreen/LoadingScreen.js
+++ src/pages/LoadingScreen/LoadingScreen.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import "./LoadingScreen.scss";
-
+import HelpIcon from '../../assets/icons/help_icon.svg'
 
 const LoadingScreen = () => {
     return (
         <main className="loadingScreen">
-            jsdlfkj
+            <section className="loadingScreen__main">
+                <p>Welcome to </p>
+                <div className="loadingScreen__main__title">
+                    <h1>Dungeon<br />Hunter</h1>
+                </div>
+            </section>
+            <section className="loadingScreen__tips-tricks">
+                <img src={HelpIcon} alt="help_icon" />
+                <p>Earn and access defi !</p>
+            </section>
         </main>
     )
 }
