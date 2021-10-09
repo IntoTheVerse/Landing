2629712b4d8446597bb8d4db87d9450e55270bbd
madrigal1 <kurupgokul11@gmail.com>
2021-10-09 21:01:23 +0530

    feat: header

--- src/pages/HomeScreen/HomeScreen.js
+++ src/pages/HomeScreen/HomeScreen.js
@@ -1,11 +1,32 @@
 
-import React from 'react'
+import React from 'react';
+import "./HomeScreen.scss";
+
+import Logo from "../../assets/logo.svg";
+import Chain from "../../assets/chain.svg"
+
 
 const HomeScreen = () => {
     return (
-        <div>
-
-        </div>
+        <article className="homeScreen">
+            <header className="homeScreen__header">
+                <div className="homeScreen__header__content">
+                    <section className="homeScreen__header__chainContainer">
+                        <img src={Chain} alt="" />
+                        <img src={Chain} alt="" />
+                    </section>
+                    <section className="homeScreen__header__main">
+                        <img src={Logo} alt="logo" />
+                        <nav className="homeScreen__header__main__nav">
+                            <p>HOME</p>
+                            <p>AbOUT</p>
+                            <p>FEATURES</p>
+                            <p>TEAMS</p>
+                        </nav>
+                    </section>
+                </div>
+            </header>
+        </article>
     )
 }
 
