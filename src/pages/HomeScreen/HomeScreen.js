4f9fb428ad9439b776a45079673ab0e8628e6723
lopeselio <lopeselio@gmail.com>
2021-10-17 03:32:09 +0530

    transform and clouds

--- src/pages/HomeScreen/HomeScreen.js
+++ src/pages/HomeScreen/HomeScreen.js
@@ -1,5 +1,6 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import "./HomeScreen.scss";
 
 import Logo from "../../assets/logo.svg";
@@ -16,38 +17,41 @@ import CloudLeftTwo from "../../assets/clouds/cloud_left_two.svg"
 
 
 const HomeScreen = () => {
+    const [isVisible, setIsVisible] = useState(true);
+    useEffect(() => {   
+        window.addEventListener("scroll", listenToScroll);
+        return () => 
+           window.removeEventListener("scroll", listenToScroll); 
+      }, [])
+
+      const listenToScroll = () => {
+        let heightToHideFrom = 500;
+        const winScroll = document.body.scrollTop || 
+            document.documentElement.scrollTop;
+           
+        if (winScroll > heightToHideFrom) { 
+           isVisible &&      // to limit setting state only the first time         
+             setIsVisible(false);
+        } else {
+             setIsVisible(true);
+        }  
+      };
     return (
-        <article className="homeScreen">
-            <header className="homeScreen__header">
-                <div className="homeScreen__header__content">
-                    <section className="homeScreen__header__chainContainer">
-                        <img src={Chain} alt="" />
-                        <img src={Chain} alt="" />
-                    </section>
-                    <section className="homeScreen__header__main">
-                        <img src={Logo} alt="logo" />
-                        <nav className="homeScreen__header__main__nav">
-                            <p>HOME</p>
-                            <p>ABOUT</p>
-                            <p>FEATURES</p>
-                            <p>TEAMS</p>
-                        </nav>
-                    </section>
-                </div>
-            </header>
+        <section className="homeScreen">
             <main className="homeScreen__bannerTitleContainer">
                 <section className="homeScreen__bannerTitleContainer__bannerTitle">
                     <h1>A PiXEL EXPERiENCE</h1>
                     <h1>PLAY, COLLECT and EARN</h1>
+                    {isVisible && <img src={PlayButton} id="playbtn" className="play play_btn_center" alt="play_button" />}
                 </section>
                 <img src={CloudLeftOne} className="cloud cloud_left_one" alt="cloud_left_one" />
                 <img src={CloudLeftTwo} className="cloud cloud_left_two" alt="cloud_left_two" />
                 <img src={CloudRightOne} className="cloud cloud_right_one" alt="cloud_right_one" />
                 <img src={CloudRightTwo} className="cloud cloud_right_two" alt="cloud_right_two" />
                 <img src={CloudRightThree} className="cloud cloud_right_three" alt="cloud_right_three" />
-                <img src={PlayButton} className="play play_btn_center" alt="play_button" />
+                
             </main>
-        </article >
+        </section >
     )
 }
 
