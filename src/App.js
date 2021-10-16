12c56736c0904926caeeb8f1ea8cc4cd75e6f0ad
Ramesh Sachan <master.ramesh96@gmail.com>
2021-10-16 22:27:08 +0530

    features bug solve

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import LoadingScreen from './pages/LoadingScreen/LoadingScreen'
 import HomeScreen from './pages/HomeScreen/HomeScreen';
 import FeaturesScreen from './pages/FeaturesScreen/FeaturesScreen';
+import TeamInfoScreen from './pages/TeamInfoScreen/TeamInfoScreen';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/" component={LoadingScreen}></Route>
         <Route exact path="/home" component={HomeScreen}></Route>
         <Route exact path="/features" component={FeaturesScreen}></Route>
+        <Route exact path="/team" component={TeamInfoScreen}></Route>
       </Switch>
     </Router>
   );
