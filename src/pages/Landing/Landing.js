df8d967ef009ca486ed14a8386663cde597cd77e
Ramesh Sachan <master.ramesh96@gmail.com>
2021-10-17 23:43:20 +0530

    footer completed

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -3,6 +3,7 @@ import HomeScreen from '../HomeScreen/HomeScreen';
 import FeaturesScreen from '../FeaturesScreen/FeaturesScreen';
 import TeamInfoScreen from '../TeamInfoScreen/TeamInfoScreen';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 const Landing = () => {
     return (
@@ -11,6 +12,7 @@ const Landing = () => {
             <HomeScreen />
             <FeaturesScreen />
             <TeamInfoScreen />
+            <Footer />
         </section>
     )
 }
