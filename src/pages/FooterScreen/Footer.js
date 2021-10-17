87eb5ee425924cdcdc1800790781c0a0c76510cf
madrigal1 <kurupgokul11@gmail.com>
2021-10-17 22:42:00 +0530

    feat: added footer fixed teams

---
+++ src/pages/FooterScreen/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Footer.scss"
+const Footer = () => {
+    return (
+        <article className="footer">
+
+        </article>
+    )
+}
+export default Footer;
--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -3,6 +3,7 @@ import HomeScreen from '../HomeScreen/HomeScreen';
 import FeaturesScreen from '../FeaturesScreen/FeaturesScreen';
 import TeamInfoScreen from '../TeamInfoScreen/TeamInfoScreen';
 import Header from '../Header/Header';
+import Footer from '../FooterScreen/Footer';
 
 const Landing = () => {
     return (
@@ -11,6 +12,7 @@ const Landing = () => {
             <HomeScreen />
             <FeaturesScreen />
             <TeamInfoScreen />
+            <Footer />
         </section>
     )
 }
